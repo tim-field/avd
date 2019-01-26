e67c5f9adad7991158f1cfc403f43ec3bd831b63
Gareth Bedford <g@thirdman.co.nz>
2019-01-26 08:45:33 +0700

    Loading icon

--- components/Loading/index.js
+++ components/Loading/index.js
@@ -1,8 +1,12 @@
 import React from "react"
+import {
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons"
 import "./Loading.scss"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Loading() {
   return <div className="Loading">
-    <p>Loading...</p>
+    <span className="iconWrap"><FontAwesomeIcon icon={faSpinner}/></span><p>Loading...</p>
   </div>
 }
