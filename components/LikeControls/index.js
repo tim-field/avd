1011e4dd20aa9c05be58ecbbda6441152716f401
Gareth Bedford <g@thirdman.co.nz>
2019-02-02 10:06:17 +0700

    highlight th eliked button

--- components/LikeControls/index.js
+++ components/LikeControls/index.js
@@ -3,14 +3,14 @@ import classNames from "classnames"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import "./LikeControls.scss"
 
-// import styles from "./LikeControls.module.scss"
+import "./LikeControls.scss"
 // note to use css modules, it would be {styles.LikeControls} (note uppercase L)
 
 export default function LikeControls({ liked, setLiked }) {
   return (
-    <div className={classNames('likeControls')}>
+    <div className={classNames("likeControls")}>
       <button
-        className={classNames({ active: liked === false })}
+        className={classNames({ active: liked === false }, "notest")}
         onClick={() => setLiked(false)}
       >
         <FontAwesomeIcon icon="thumbs-down" />
